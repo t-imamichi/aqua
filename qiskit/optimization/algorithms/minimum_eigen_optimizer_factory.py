e8ad217a47129612bdd40f119ee3cd67a57d7012
Takashi Imamichi <imamichi@jp.ibm.com>
2020-12-15 17:49:59 +0900

    add comments

--- qiskit/optimization/algorithms/minimum_eigen_optimizer_factory.py
+++ qiskit/optimization/algorithms/minimum_eigen_optimizer_factory.py
@@ -3,6 +3,7 @@ import numpy as np
 from qiskit.aqua import AquaError
 from qiskit.aqua.algorithms.minimum_eigen_solvers import (MinimumEigensolver,
                                                           NumPyMinimumEigensolver, VQE, QAOA)
+from qiskit.circuit.library import EfficientSU2
 from . import MinimumEigenOptimizer
 from ..converters import QuadraticProgramToQubo
 from ..problems import QuadraticProgram
@@ -13,45 +14,49 @@ class MinimumEigenOptimizerFactory:
         self._mes = mes
 
     def get_solver(self, prob: QuadraticProgram) -> MinimumEigenOptimizer:
-        mes = self._get_mes()
+        mes = self._get_mes(prob)
         return MinimumEigenOptimizer(min_eigen_solver=mes)
 
-    def _get_mes(self) -> MinimumEigensolver:
+    def _get_mes(self, prob: QuadraticProgram) -> MinimumEigensolver:
         if self._mes == 'numpy':
-            mes = self._get_numpy_mes()
+            mes = self._get_numpy_mes(prob)
         elif self._mes == 'vqe':
-            mes = self._get_vqe()
+            mes = self._get_vqe(prob)
         elif self._mes == 'qaoa':
-            mes = self._get_qaoa()
+            mes = self._get_qaoa(prob)
         else:
             raise AquaError('Invalid mes name: {}. Should be "numpy", "vqe", or "qaoa".'
                             ''.format(self._mes))
         return mes
 
-    def _get_numpy_mes(self) -> NumPyMinimumEigensolver:
+    def _get_numpy_mes(self, prob: QuadraticProgram) -> NumPyMinimumEigensolver:
         return NumPyMinimumEigensolver()
 
-    def _get_vqe(self) -> VQE:
+    def _get_vqe(self, prob: QuadraticProgram) -> VQE:
         return VQE()
 
-    def _get_qaoa(self) -> QAOA:
+    def _get_qaoa(self, prob: QuadraticProgram) -> QAOA:
         return QAOA()
 
 
-# example to customize a factory
-class SpecialMinimumEigenOptimizerFactory(MinimumEigenOptimizerFactory):
+class SampleMEOFactory(MinimumEigenOptimizerFactory):
     def __init__(self, penalty: float, initial_point: np.ndarray, mes: str = 'numpy'):
         super().__init__(mes)
         self._penalty = penalty
         self._initial_point = initial_point
 
     def get_solver(self, prob: QuadraticProgram) -> MinimumEigenOptimizer:
-        mes = self._get_mes()
+        mes = self._get_mes(prob)
+        # generate a custom list of converters
         converters = [QuadraticProgramToQubo(penalty=self._penalty)]
         return MinimumEigenOptimizer(min_eigen_solver=mes, converters=converters)
 
-    def _get_vqe(self) -> VQE:
-        return VQE(initial_point=self._initial_point)
+    def _get_vqe(self, prob: QuadraticProgram) -> VQE:
+        # generate a custom varform
+        var_form = EfficientSU2(reps=1)
+        return VQE(initial_point=self._initial_point, var_form=var_form)
 
-    def _get_qaoa(self) -> QAOA:
-        return QAOA(initial_point=self._initial_point)
+    def _get_qaoa(self, prob: QuadraticProgram) -> QAOA:
+        # customize the p-value
+        p = 2
+        return QAOA(p=p, initial_point=self._initial_point)
